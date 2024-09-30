7e53dbd9e0da07a241dbe99221d638c777f43a18
Eric Pinzur <epinzur@gmail.com>
2024-09-30 22:32:56 +0200

    fixed asset loading issues

--- demos/src/levels_setup/for_3d_platformer.rs
+++ demos/src/levels_setup/for_3d_platformer.rs
@@ -27,10 +27,15 @@ pub fn setup_level(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    asset_server: Res<AssetServer>
+    asset_server: Res<AssetServer>,
 ) {
-    let is_spherical = false;
+    let is_spherical = true;
     println!("Setting up level as spherical: {:?}", is_spherical);
+
+    fn get_transform(position: Vec3, is_spherical: bool) -> Transform {
+        let transform = Transform::from_translation(position);
+        adjust_transform(transform, is_spherical)
+    }
 
     // Helper functions to adjust positions and transforms
     fn adjust_position(mut position: Vec3, is_spherical: bool) -> Vec3 {
@@ -170,15 +175,11 @@ pub fn setup_level(
     // Fall-through platforms
     let fall_through_obstacles_material = materials.add(Color::from(css::PINK).with_alpha(0.8));
     for (i, y) in [2.0, 4.5].into_iter().enumerate() {
-        let position = Vec3::new(6.0, y, 10.0);
-        let mut transform = Transform::from_translation(position);
-        transform = adjust_transform(transform, is_spherical);
-
         let mut cmd = commands.spawn((LevelObject, Name::new(format!("Fall Through #{}", i + 1))));
         cmd.insert(PbrBundle {
             mesh: meshes.add(Cuboid::new(6.0, 0.5, 2.0)),
             material: fall_through_obstacles_material.clone(),
-            transform,
+            transform: get_transform(Vec3::new(6.0, y, 10.0), is_spherical),
             ..Default::default()
         });
         #[cfg(feature = "rapier3d")]
@@ -202,73 +203,71 @@ pub fn setup_level(
     }
 
     // Adjust and spawn other objects similarly
-    let scene_positions = [
-        ("Collision Groups", Vec3::new(10.0, 2.0, 1.0)),
-        ("Sensor", Vec3::new(20.0, 2.0, 1.0)),
-    ];
-
-    for (name, position) in scene_positions.iter() {
-        let mut transform = Transform::from_translation(*position);
-        transform = adjust_transform(transform, is_spherical);
-
-        println!("Loading asset scene bundle: {:?}", name);
-
-        let mut cmd = commands.spawn((
-            LevelObject,
-            Name::new(*name),
-            SceneBundle {
-                scene: asset_server.load(&format!(
-                    "{}.glb#Scene0",
-                    name.to_lowercase().replace(" ", "-")
-                )),
-                transform,
-                ..Default::default()
+    commands.spawn((
+        LevelObject,
+        Name::new("Collision Groups"),
+        SceneBundle {
+            scene: asset_server.load("collision-groups-text.glb#Scene0"),
+            transform: get_transform(Vec3::new(10.0, 2.0, 1.0), is_spherical), // .with_scale(0.01 * Vec3::ONE),
+            ..Default::default()
+        },
+        #[cfg(feature = "rapier3d")]
+        (
+            rapier::Collider::cuboid(2.0, 1.0, 2.0),
+            CollisionGroups {
+                memberships: Group::GROUP_1,
+                filters: Group::GROUP_1,
             },
-        ));
+        ),
+        #[cfg(feature = "avian3d")]
+        (
+            avian::RigidBody::Static,
+            avian::Collider::cuboid(4.0, 2.0, 4.0),
+            CollisionLayers::new([LayerNames::PhaseThrough], [LayerNames::PhaseThrough]),
+        ),
+    ));
 
-        if *name == "Collision Groups" {
-            #[cfg(feature = "rapier3d")]
-            {
-                cmd.insert(rapier::Collider::cuboid(2.0, 1.0, 2.0));
-                cmd.insert(CollisionGroups {
-                    memberships: Group::GROUP_1,
-                    filters: Group::GROUP_1,
-                });
-            }
-            #[cfg(feature = "avian3d")]
-            {
-                cmd.insert(avian::RigidBody::Static);
-                cmd.insert(avian::Collider::cuboid(4.0, 2.0, 4.0));
-                cmd.insert(CollisionLayers::new(
-                    [LayerNames::PhaseThrough],
-                    [LayerNames::PhaseThrough],
-                ));
-            }
-        } else if *name == "Sensor" {
-            #[cfg(feature = "rapier3d")]
-            {
-                cmd.insert(rapier::Collider::cuboid(2.0, 1.0, 2.0));
-                cmd.insert(rapier::Sensor);
-            }
-            #[cfg(feature = "avian3d")]
-            {
-                cmd.insert(avian::RigidBody::Static);
-                cmd.insert(avian::Collider::cuboid(4.0, 2.0, 4.0));
-                cmd.insert(avian::Sensor);
-            }
-        }
-    }
+    #[cfg(feature = "rapier3d")]
+    commands.spawn((
+        LevelObject,
+        Name::new("Solver Groups"),
+        SceneBundle {
+            scene: asset_server.load("solver-groups-text.glb#Scene0"),
+            transform: get_transform(Vec3::new(15.0, 2.0, 1.0), is_spherical), // .with_scale(0.01 * Vec3::ONE),
+            ..Default::default()
+        },
+        rapier::Collider::cuboid(2.0, 1.0, 2.0),
+        SolverGroups {
+            memberships: Group::GROUP_1,
+            filters: Group::GROUP_1,
+        },
+    ));
+
+    commands.spawn((
+        LevelObject,
+        Name::new("Sensor"),
+        SceneBundle {
+            scene: asset_server.load("sensor-text.glb#Scene0"),
+            transform: get_transform(Vec3::new(20.0, 2.0, 1.0), is_spherical), // .with_scale(0.01 * Vec3::ONE),
+            ..Default::default()
+        },
+        #[cfg(feature = "rapier3d")]
+        (rapier::Collider::cuboid(2.0, 1.0, 2.0), rapier::Sensor),
+        #[cfg(feature = "avian3d")]
+        (
+            avian::RigidBody::Static,
+            avian::Collider::cuboid(4.0, 2.0, 4.0),
+            avian::Sensor,
+        ),
+    ));
 
     // Spawn moving platform
     {
-        let mut transform = Transform::from_xyz(-4.0, 6.0, 0.0);
-        transform = adjust_transform(transform, is_spherical);
-
         let mut cmd = commands.spawn((LevelObject, Name::new("Moving Platform")));
         cmd.insert(PbrBundle {
             mesh: meshes.add(Cuboid::new(4.0, 1.0, 4.0)),
             material: materials.add(Color::from(css::BLUE)),
-            transform,
+            transform: get_transform(Vec3::new(-4.0, 6.0, 0.0), is_spherical),
             ..Default::default()
         });
         #[cfg(feature = "rapier3d")]
@@ -296,8 +295,7 @@ pub fn setup_level(
 
     // Spawn spinning platform
     {
-        let mut transform = Transform::from_xyz(-2.0, 2.0, 10.0);
-        transform = adjust_transform(transform, is_spherical);
+        let transform = get_transform(Vec3::new(-2.0, 2.0, 10.0), is_spherical);
         let direction = if is_spherical { transform.translation.normalize() } else {Vec3::Y};
 
         let mut cmd = commands.spawn((LevelObject, Name::new("Spinning Platform")));
